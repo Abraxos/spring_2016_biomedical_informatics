ed5de892d3979e2060bf31d0dfcd7584ac7f10aa
Ivan Pozdnyakov <ivan@starscream.engr.uconn.edu>
2016-04-10 15:41:59 -0400

    finished the converter! haven't written the docs on it.

--- ivan/converter/converter.py
+++ ivan/converter/converter.py
@@ -12,7 +12,7 @@ try:
 except:
 	print "I am unable to connect to the database."
 
-def convert(table):
+def convert(table,first_run):
 	# Execute Query
 	cur = conn.cursor()
 	try:
@@ -29,14 +29,14 @@ def convert(table):
 	
 	# for dev (can delete after)
 #	print(colnames)
-#	print(rows[0])
 #	types = []
 #	for attr in rows[0]:
 #		types.append(type(attr))
 #	print(types)
 	
 	# Establish a new hyperdex space
-	a.rm_space(table)
+	if(not first_run):
+		a.rm_space(table)
 	if(table == "resource_compartment"):
 		s = 'space '+table+' '+\
 		'key '+colnames[1] +' '+\
@@ -50,42 +50,102 @@ def convert(table):
                 s = 'space '+table+' '+\
                 'key '+colnames[0] +' '+\
                 'attributes string '+colnames[1]+', string '+colnames[2]+', string '+colnames[3]+', int '+\
-		colnames[4]+', string '+colnames[5]+', int '+colnames[6]+', int '+colnames[7]+', string '+\
+		colnames[4]+', string '+colnames[5]+', float '+colnames[6]+', float '+colnames[7]+', string '+\
 		colnames[8]+', string '+colnames[9]+', string '+colnames[10]+', string '+colnames[11]+', string '+\
 		colnames[12]+', string '+colnames[13]+', string '+colnames[14]+', string '+colnames[15]+', string '+\
-		colnames[16]+', timestamp(second) '+colnames[17]+', timestamp(second) '+colnames[18]+' '+\
+		colnames[16]+', timestamp(minute) '+colnames[17]+', timestamp(minute) '+colnames[18]+' '+\
                 'subspace '+colnames[1]+' '+\
                 'create 8 partitions tolerate 2 failures'
 		a.add_space(s)
                 for row in rows:
-			replacements = [row[0],row[4],row[6],row[7]]
-			if replacements[0] is not None:
-				replacements[0] = int(replacements[0])
-			if replacements[1] is not None:
-				replacements[1] = int(replacements[1])
-			if replacements[2] is not None:
-				replacements[2] = int(replacements[2])
-			if replacements[3] is not None:
-				replacements[3] = int(replacements[3])
+			long_replacements = [row[0],row[4]]
+			
+			for idx,entry in enumerate(long_replacements):
+				if long_replacements[idx] is not None:
+					long_replacements[idx] = int(entry)
+				else:
+					long_replacements[idx] = 0
+			
+			real_replacements = [row[6],row[7]]
+			
+			for idx,entry in enumerate(real_replacements):
+                                if real_replacements[idx] is not None:
+                                        real_replacements[idx] = float(entry)
+                                else:
+                                        real_replacements[idx] = 0.0
 
-                        c.put(table,replacements[0], {colnames[1]:row[1], colnames[2]:row[2],
-			colnames[3]:row[3], colnames[4]:replacements[1], colnames[5]:row[5],			
-			colnames[6]:replacements[2], colnames[7]:replacements[3], colnames[8]:row[8],
-			colnames[9]:row[9], colnames[10]:row[10], colnames[11]:row[11],
-			colnames[12]:row[12], colnames[13]:row[13], colnames[14]:row[14],
-			colnames[15]:row[15], colnames[16]:row[16], colnames[17]:row[17],
-			colnames[18]:row[18]})		
+			string_replacements = [row[1],row[2],row[3],row[5],
+			row[8],row[9],row[10],row[11],row[12],row[13],row[14],
+			row[15],row[16]]
+
+			for idx,entry in enumerate(string_replacements):
+				if string_replacements[idx] is None:
+					string_replacements[idx] = "isNone"
+			
+			date_replacements = [row[17],row[18]]
+			for idx,entry in enumerate(date_replacements):
+				if date_replacements[idx] is None:
+					date_replacements[idx] = datetime.datetime(1970, 1, 1, 0, 0)
+
+			c.put(table,long_replacements[0], {colnames[1]:string_replacements[0], colnames[2]:string_replacements[1],
+			colnames[3]:string_replacements[2], colnames[4]:long_replacements[1], colnames[5]:string_replacements[3],
+			colnames[6]:real_replacements[0], colnames[7]:real_replacements[1], colnames[8]:string_replacements[4],
+			colnames[9]:string_replacements[5], colnames[10]:string_replacements[6], colnames[11]:string_replacements[7],
+			colnames[12]:string_replacements[8], colnames[13]:string_replacements[9], colnames[14]:string_replacements[10],
+			colnames[15]:string_replacements[11], colnames[16]:string_replacements[12], 
+			colnames[17]:date_replacements[0], colnames[18]:date_replacements[1]})		
 	elif(table == "resource_version"):
-		print("_")
+		s = 'space '+table+' '+\
+		'key '+colnames[0]+' '+\
+		'attributes string '+colnames[1]+', string '+colnames[2]+', string '+colnames[3]+', timestamp(minute)'+\
+		colnames[4]+', string '+colnames[5]+' '+\
+		'subspace '+colnames[1]+' '+\
+		'create 8 partitions tolerate 2 failures'
+		a.add_space(s)
+		for row in rows:
+			long_replacements = [row[0]]
+
+			for idx,entry in enumerate(long_replacements):
+				long_replacements[idx] = int(entry)
+			
+			c.put(table,long_replacements[0], {colnames[1]:row[1], colnames[2]:row[2],
+			colnames[3]:row[3], colnames[4]:row[4], colnames[5]:row[5]})
 	elif(table == "launch_context"):
-		print("_")
+		s = 'space '+table+' '+\
+		'key '+colnames[0]+' '+\
+		'attributes string '+colnames[1]+', timestamp(minute) '+colnames[2]+', string '+colnames[3]+', string '+\
+		colnames[4]+' '+\
+		'subspace '+colnames[1]+' '+\
+		'create 8 partitions tolerate 2 failures'
+		a.add_space(s)
+		for row in rows:
+			print(row)
+			long_replacements = [row[0]]
+			for idx,entry in enumerate(long_replacements):
+				long_replacements[idx] = int(entry)
+
+			c.put(table,long_replacements[0], {colnames[1]:row[1], colnames[2]:row[2],
+			colnames[3]:row[3], colnames[4]:row[4]})
 	elif(table == "launch_context_params"):
-		print("_")
+		s = 'space '+table+' '+\
+		'key '+colnames[0]+' '+\
+		'attributes int '+colnames[1]+', string '+colnames[2]+', string '+colnames[3]+' '+\
+		'subspace '+colnames[1]+' '+\
+		'create 8 partitions tolerate 2 failures'
+		a.add_space(s)
+		for row in rows:
+			long_replacements = [row[0]]
+			for idx,entry in enumerate(long_replacements):
+				long_replacements[idx] = int(entry)
+
+			c.put(table,long_replacements[0], {colnames[1]:long_replacements[1], colnames[2]:row[2],
+			colnames[3]:row[3]})
+
 	else:
 		print("no conversion of this table")
 
-#convert("resource_compartment")
-convert("resource_index_term")
-#convert("resource_version")
-#convert("launch_context")
-#convert("launch_context_params")
+#convert("resource_compartment",False)
+#convert("resource_index_term",False)
+#convert("resource_version",False)
+#convert("launch_context",False)
+convert("launch_context_params",False)
